fa0bb19840b6e7e4b15e73d929b51404fadba66d
Brobert04 <robert.bercaru@liceuldearte.ro>
2023-03-28 17:01:32 +0300

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,10 @@ class AppServiceProvider extends ServiceProvider
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
-    
+        $this->app->bind('path.public', function() {
+            return base_path().'/../public_html';
+        });
+
     }
 }
     
